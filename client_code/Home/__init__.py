10e0fabc962f05988a2c9c120894dc2f57607680
blackforestlphd@gmail.com <blackforestlphd@gmail.com>
2024-10-29 10:38:14 +0000

    Created Home; Edited settings, Settings, InOut and Insights; Deleted Now

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import NowTemplate
+from ._anvil_designer import HomeTemplate
 from anvil import *
 import anvil.tables as tables
 import anvil.tables.query as q
@@ -7,7 +7,7 @@ import anvil.server
 import plotly.graph_objects as go
 
 
-class Now(NowTemplate):
+class Home(HomeTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
