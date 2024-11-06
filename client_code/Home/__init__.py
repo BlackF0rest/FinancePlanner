d1b0c98365029d9b4eb53f9531297f695b2393cb
blackforestlphd@gmail.com <blackforestlphd@gmail.com>
2024-11-06 13:00:01 +0000

    Created server_code; Edited Expense, Home, Home.ItemTemplate4, Income and 8 more

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -1,5 +1,8 @@
 from ._anvil_designer import HomeTemplate
 from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
@@ -11,6 +14,7 @@ class Home(HomeTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.user = anvil.users.login_with_form()
 
     # Any code you write here will run before the form opens.
 
