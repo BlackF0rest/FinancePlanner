9ffe5cec52b5ba61548f8ef9bf6b818bd07a9e8e
blackforestlphd@gmail.com <blackforestlphd@gmail.com>
2024-11-19 10:32:05 +0000

    Edited Income and Transfer; Deleted Income.ItemTemplate5 and Income.ItemTemplate6

--- client_code/Income/__init__.py
+++ client_code/Income/__init__.py
@@ -15,82 +15,155 @@ class Income(IncomeTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    self.set_event_handler('x-set-icon', self.set_selected_icon)
+    self.set_event_handler("x-set-icon", self.set_selected_icon)
 
-    self.rppn_icons.items = anvil.server.call('get_all_icons')
+    self.rppn_icons.items = anvil.server.call("get_all_icons")
+
+    self.dt_main.date = datetime.now().date()
+    self.dt_recurring.min_date = self.dt_main.date + timedelta(days=1)
+    self.dt_spreadout.min_date = self.dt_main.date + timedelta(days=1)
 
     self.selected_icon = None
 
-  def outlined_button_2_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    open_form('Home')
-
   def bt_add_click(self, **event_args):
     """This method is called when the button is clicked"""
-    if not self.input_numb.text :
+    if not self.input_numb.text:
       self.input_numb.border = "2px solid red"
     elif not self.input_name.text:
       self.input_name.border = "2px solid red"
     elif not self.selected_icon:
       self.img_icon.border = "2px solid red"
+    elif not self.dt_main.date:
+      self.dt_main.border = "2px solid red"
     else:
-      if self.rd_recurring.value:
-        pass
-      elif self.rd_spreadout.value:
-        today = datetime.now().date()
-        end_date = self.dt_spreadout.date
-        total_value = float(self.input_numb.text)
-        daily_value = total_value / (end_date-today)
-        print(daily_value)
-      #anvil.server.call('write_transaction', type='expense', category=self.selected_icon, amount=float(self.input_numb.text), name=self.input_name.text, account_id=anvil.server.call('get_current_account_id', anvil.users.get_user()))
-      open_form('Home')
+      if self.rd_recurring.selected:
+        if not self.dt_recurring.date:
+          self.dt_recurring.border = "2px solid red"
+        else:
+          today = self.dt_main.date
+          recurring_days = (self.dt_recurring.date - today).days
+          total_value = float(self.input_numb.text)
+          daily_value = round((total_value / recurring_days), 2)
+          end_date = self.dt_end_recurring.date
+          anvil.server.call(
+            "write_transaction",
+            type="income",
+            date=today,
+            category=self.selected_icon,
+            amount=daily_value,
+            name=self.input_name.text,
+            recurring=True,
+            end_date=end_date,
+            account_id=anvil.server.call(
+              "get_current_account_id", anvil.users.get_user()
+            ),
+          )
+      elif self.rd_spreadout.selected:
+        if not self.rd_spreadout.date:
+          self.rd_spreadout.border = "2px solid red"
+        else:
+          today = self.dt_main.date
+          end_date = self.dt_spreadout.date
+          total_value = float(self.input_numb.text)
+          daily_value = round((total_value / (end_date - today).days), 2)
+          anvil.server.call(
+            "write_transaction",
+            type="income",
+            date=today,
+            category=app_tables.icons.get(category=self.selected_icon),
+            amount=daily_value,
+            name=self.input_name.text,
+            recurring=True,
+            end_date=end_date,
+            account_id=anvil.server.call(
+              "get_current_account_id", anvil.users.get_user()
+            ),
+          )
+      else:
+        anvil.server.call(
+          "write_transaction",
+          type="income",
+          date=today,
+          category=app_tables.icons.get(category=self.selected_icon),
+          amount=float(self.input_numb.text),
+          name=self.input_name.text,
+          account_id=anvil.server.call(
+            "get_current_account_id", anvil.users.get_user()
+          ),
+        )
+      open_form("Home")
+
+  def outlined_button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("Home")
 
   def rd_recurring_change(self, **event_args):
     """This method is called when this radio button is selected (but not deselected)"""
-    self.date_picker_1.visible = self.radio_button_1.selected
-    self.date_picker_1_copy.visible = False
-    self.radio_button_2.selected != self.radio_button_1.selected
-
-  def rd_spreadout_change(self, **event_args):
-    """This method is called when this radio button is selected (but not deselected)"""
-    self.date_picker_1_copy.visible = self.radio_button_2.selected
-    self.date_picker_1.visible = False
-    self. radio_button_1.selected != self.radio_button_2.selected
+    pass
 
   def set_selected_icon(self, **event_args):
-      self.selected_icon = event_args['icon_category']
-      self.rppn_icons.visible = False
-      self.img_icon.source = anvil.server.call('get_icon', event_args['icon_category'])
-      self.img_icon.border = ''
+    self.selected_icon = event_args["icon_category"]
+    self.rppn_icons.visible = False
+    self.img_icon.source = anvil.server.call("get_icon", event_args["icon_category"])
+    self.img_icon.border = ""
 
   def bt_set_icon_click(self, **event_args):
     """This method is called when the button is clicked"""
     self.rppn_icons.visible = True
 
-  def radio_button_1_clicked(self, **event_args):
+  def rd_recurring_clicked(self, **event_args):
     """This method is called when this radio button is selected"""
     self.rd_recurring.selected != self.rd_recurring.selected
     self.dt_spreadout.visible = False
+    self.lb_spreadout.visible = False
     self.dt_recurring.visible = self.rd_recurring.selected
+    self.lb_recurring.visible = self.rd_recurring.selected
+    self.dt_end_recurring.visible = self.rd_recurring.selected
+    self.lb_end_recurring.visible = self.rd_recurring.selected
 
-  def radio_button_2_clicked(self, **event_args):
+  def rd_spreadout_clicked(self, **event_args):
     """This method is called when this radio button is selected"""
     self.rd_spreadout.selected != self.rd_spreadout.selected
     self.dt_recurring.visible = False
-    self.dt_start_spreadout.visible = self.rd_spreadout.selected
-    self.dt_end_spreadout.visible = self.rd_spreadout.selected
+    self.lb_recurring.visible = False
+    self.dt_end_recurring.visible = False
+    self.lb_end_recurring.visible = False
+    self.dt_spreadout.visible = self.rd_spreadout.selected
+    self.lb_spreadout.visible = self.rd_spreadout.selected
 
-  def rb_one_time_clicked(self, **event_args):
+  def rd_one_time_clicked(self, **event_args):
     """This method is called when this radio button is selected"""
     self.dt_recurring.visible = False
+    self.lb_recurring.visible = False
     self.dt_spreadout.visible = False
+    self.lb_spreadout.visible = False
+    self.dt_end_recurring.visible = False
+    self.lb_end_recurring.visible = False
 
   def input_numb_change(self, **event_args):
     """This method is called when the text in this text box is edited"""
-    if self.input_numb.border != '':
-      self.input_numb.border = ''
+    if self.input_numb.border != "":
+      self.input_numb.border = ""
 
   def input_name_change(self, **event_args):
     """This method is called when the text in this text box is edited"""
-    if self.input_name.border != '':
-      self.input_name.border = ''
+    if self.input_name.border != "":
+      self.input_name.border = ""
+
+  def dt_main_change(self, **event_args):
+    """This method is called when the selected date changes"""
+    self.dt_recurring.min_date = self.dt_main.date + timedelta(days=1)
+    self.dt_spreadout.min_date = self.dt_main.date + timedelta(days=1)
+    if self.dt_main.border != "":
+      self.dt_main.border = ""
+
+  def dt_recurring_change(self, **event_args):
+    """This method is called when the selected date changes"""
+    self.dt_end_recurring.min_date = self.dt_recurring.date + timedelta(days=1)
+    if self.dt_recurring.border != "":
+      self.dt_recurring.border = ""
+
+  def dt_spreadout_change(self, **event_args):
+    """This method is called when the selected date changes"""
+    if self.dt_spreadout.border != "":
+      self.dt_spreadout.border = ""
--- client_code/Transfer/__init__.py
+++ client_code/Transfer/__init__.py
@@ -7,6 +7,7 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from datetime import datetime, timedelta, date
 
 
 class Transfer(TransferTemplate):
@@ -14,17 +15,172 @@ class Transfer(TransferTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
+    self.set_event_handler("x-set-icon", self.set_selected_icon)
+
+    self.rppn_icons.items = anvil.server.call("get_all_icons")
+
+    self.dt_main.date = datetime.now().date()
+    self.dt_recurring.min_date = self.dt_main.date + timedelta(days=1)
+    self.dt_spreadout.min_date = self.dt_main.date + timedelta(days=1)
+
+    self.selected_icon = None
+
+    self.accounts = None
+
+    self.update_accounts()
+
+  def update_accounts(self):
+    self.accounts = anvil.server.call('get_user_accounts')
+
+    excluded_account_id = anvil.server.call('get_current_account_id')  # Replace with the account ID you want to exclude
+
+    self.dp_accounts.items = [
+
+        (account['name'], account['id']) for account in self.accounts
+    
+            if account['id'] != excluded_account_id
+    
+    ]
+  
+  def bt_add_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    if not self.input_numb.text:
+      self.input_numb.border = "2px solid red"
+    elif not self.input_name.text:
+      self.input_name.border = "2px solid red"
+    elif not self.selected_icon:
+      self.img_icon.border = "2px solid red"
+    elif not self.dt_main.date:
+      self.dt_main.border = "2px solid red"
+    else:
+      if self.rd_recurring.selected:
+        if not self.dt_recurring.date:
+          self.dt_recurring.border = "2px solid red"
+        else:
+          today = self.dt_main.date
+          recurring_days = (self.dt_recurring.date - today).days
+          total_value = float(self.input_numb.text)
+          daily_value = round((total_value / recurring_days), 2)
+          end_date = self.dt_end_recurring.date
+          anvil.server.call(
+            "write_transaction",
+            type="expense",
+            date=today,
+            category=self.selected_icon,
+            amount=daily_value,
+            name=self.input_name.text,
+            recurring=True,
+            end_date=end_date,
+            account_id=anvil.server.call(
+              "get_current_account_id", anvil.users.get_user()
+            ),
+          )
+      elif self.rd_spreadout.selected:
+        if not self.rd_spreadout.date:
+          self.rd_spreadout.border = "2px solid red"
+        else:
+          today = self.dt_main.date
+          end_date = self.dt_spreadout.date
+          total_value = float(self.input_numb.text)
+          daily_value = round((total_value / (end_date - today).days), 2)
+          anvil.server.call(
+            "write_transaction",
+            type="expense",
+            date=today,
+            category=app_tables.icons.get(category=self.selected_icon),
+            amount=daily_value,
+            name=self.input_name.text,
+            recurring=True,
+            end_date=end_date,
+            account_id=anvil.server.call(
+              "get_current_account_id", anvil.users.get_user()
+            ),
+          )
+      else:
+        anvil.server.call(
+          "write_transaction",
+          type="expense",
+          date=today,
+          category=app_tables.icons.get(category=self.selected_icon),
+          amount=float(self.input_numb.text),
+          name=self.input_name.text,
+          account_id=anvil.server.call(
+            "get_current_account_id", anvil.users.get_user()
+          ),
+        )
+      open_form("Home")
 
   def outlined_button_2_click(self, **event_args):
     """This method is called when the button is clicked"""
-    open_form('Home')
+    open_form("Home")
 
-  def outlined_button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    open_form('Home')
-
-  def radio_button_1_change(self, **event_args):
+  def rd_recurring_change(self, **event_args):
     """This method is called when this radio button is selected (but not deselected)"""
-     self.date_picker_1.visible = self.radio_button_1.selected
-     self.date_picker_1.visible = self.radio_button_1.selected
+    pass
+
+  def set_selected_icon(self, **event_args):
+    self.selected_icon = event_args["icon_category"]
+    self.rppn_icons.visible = False
+    self.img_icon.source = anvil.server.call("get_icon", event_args["icon_category"])
+    self.img_icon.border = ""
+
+  def bt_set_icon_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.rppn_icons.visible = True
+
+  def rd_recurring_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    self.rd_recurring.selected != self.rd_recurring.selected
+    self.dt_spreadout.visible = False
+    self.lb_spreadout.visible = False
+    self.dt_recurring.visible = self.rd_recurring.selected
+    self.lb_recurring.visible = self.rd_recurring.selected
+    self.dt_end_recurring.visible = self.rd_recurring.selected
+    self.lb_end_recurring.visible = self.rd_recurring.selected
+
+  def rd_spreadout_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    self.rd_spreadout.selected != self.rd_spreadout.selected
+    self.dt_recurring.visible = False
+    self.lb_recurring.visible = False
+    self.dt_end_recurring.visible = False
+    self.lb_end_recurring.visible = False
+    self.dt_spreadout.visible = self.rd_spreadout.selected
+    self.lb_spreadout.visible = self.rd_spreadout.selected
+
+  def rd_one_time_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    self.dt_recurring.visible = False
+    self.lb_recurring.visible = False
+    self.dt_spreadout.visible = False
+    self.lb_spreadout.visible = False
+    self.dt_end_recurring.visible = False
+    self.lb_end_recurring.visible = False
+
+  def input_numb_change(self, **event_args):
+    """This method is called when the text in this text box is edited"""
+    if self.input_numb.border != "":
+      self.input_numb.border = ""
+
+  def input_name_change(self, **event_args):
+    """This method is called when the text in this text box is edited"""
+    if self.input_name.border != "":
+      self.input_name.border = ""
+
+  def dt_main_change(self, **event_args):
+    """This method is called when the selected date changes"""
+    self.dt_recurring.min_date = self.dt_main.date + timedelta(days=1)
+    self.dt_spreadout.min_date = self.dt_main.date + timedelta(days=1)
+    if self.dt_main.border != "":
+      self.dt_main.border = ""
+
+  def dt_recurring_change(self, **event_args):
+    """This method is called when the selected date changes"""
+    self.dt_end_recurring.min_date = self.dt_recurring.date + timedelta(days=1)
+    if self.dt_recurring.border != "":
+      self.dt_recurring.border = ""
+
+  def dt_spreadout_change(self, **event_args):
+    """This method is called when the selected date changes"""
+    if self.dt_spreadout.border != "":
+      self.dt_spreadout.border = ""
