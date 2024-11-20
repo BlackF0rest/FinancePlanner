9d22a2698bfb959b7c86f4b8c8b2f45fcd5b5c16
blackforestlphd@gmail.com <blackforestlphd@gmail.com>
2024-11-20 23:12:53 +0000

    Edited Transactions.rppn_transactions

--- client_code/Transactions/rppn_transactions/__init__.py
+++ client_code/Transactions/rppn_transactions/__init__.py
@@ -16,7 +16,7 @@ class rppn_transactions(rppn_transactionsTemplate):
 
     self.lb_name.text = self.item['name']
     self.lb_amount.text = f"${self.item['amount']:.2f}"
-    self.img_icon.source = anvil.server.call('get_icon', self.item['Category'])
+    self.img_icon.source = self.item['category']['Icon']
     
     if 'to' in self.item :
       self.lb_to.visible = True
