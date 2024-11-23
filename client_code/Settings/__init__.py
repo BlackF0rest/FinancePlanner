2b1060a7f7c2f8efdca633a3f05b5a545951951f
blackforestlphd@gmail.com <blackforestlphd@gmail.com>
2024-11-23 22:00:34 +0000

    Edited Settings

--- client_code/Settings/__init__.py
+++ client_code/Settings/__init__.py
@@ -14,14 +14,10 @@ class Settings(SettingsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.settings = None
+    self.selected_currency = None
+    self.selected_days = None
     self.update_days()
-    
-    selected_currency = anvil.server.call('get_currency')
-    if selected_currency == '€':
-      self.rd_euro.selected = True
-    else:
-      self.rd_dollar.selected = True
+    self.update_currency()
     
   def plot_now_show(self, **event_args):
     """This method is called when the Plot is shown on the screen"""
@@ -39,34 +35,40 @@ class Settings(SettingsTemplate):
     """This method is called when the button is clicked"""
     open_form('Insights')
 
-  def button_setting_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    open_form('Settings')
+  def update_days(self):
+    self.selected_days = anvil.server.call('get_settings')['max_days_ahead_from_today']
+    self.lb_days_future.text = self.selected_days
+
+  def update_currency(self):
+    self.selected_currency =  anvil.server.call('get_currency')
+    if self.selected_currency == '€':
+      self.rd_euro.selected = True
+    else:
+      self.rd_dollar.selected = True
 
   def bt_plus_days_click(self, **event_args):
     """This method is called when the button is clicked"""
-    days = self.settings['max_days_ahead_from_today'] + 1
-    anvil.server.call('set_days_into_future', anvil.users.get_user(), days)
-    self.update_days()
-
-  def update_days(self):
-    self.settings = anvil.server.call('get_settings', user=anvil.users.get_user())
-    self.lb_days_future.text = self.settings['max_days_ahead_from_today']
+    self.selected_days += 1
+    self.lb_days_future.text = self.selected_days
 
   def bt_minus_days_click(self, **event_args):
     """This method is called when the button is clicked"""
-    days = self.settings['max_days_ahead_from_today'] - 1
-    anvil.server.call('set_days_into_future', anvil.users.get_user(), days)
-    self.update_days()
+    self.selected_days -= 1
+    self.lb_days_future.text = self.selected_days
 
   def rd_dollar_clicked(self, **event_args):
     """This method is called when this radio button is selected"""
-    anvil.server.call('set_currency', '$')
+    self.selected_currency = '$'
 
   def rd_euro_clicked(self, **event_args):
     """This method is called when this radio button is selected"""
-    anvil.server.call('set_currency', '€')
+    self.selected_currency = '€'
 
-  def bt_request_feature_click(self, **event_args):
+  def bt_cancel_click(self, **event_args):
     """This method is called when the button is clicked"""
-    pass
+    self.update_currency()
+
+  def bt_apply_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.server.call('set_days_into_future', self.selected_days)
+    anvil.server.call('set_currency', self.selected_currency)
