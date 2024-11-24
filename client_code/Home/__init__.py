07be91f22e2023a8620988db49804105fcf0e694
blackforestlphd@gmail.com <blackforestlphd@gmail.com>
2024-11-24 22:24:55 +0000

    Edited Transactions, Settings and Home

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -1,7 +1,6 @@
 from ._anvil_designer import HomeTemplate
 from anvil import *
-import anvil.google.auth
-import anvil.google.drive
+import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 import anvil.users
 import anvil.tables as tables
@@ -11,150 +10,146 @@ import anvil.server
 import plotly.graph_objects as go
 
 class Home(HomeTemplate):
-    """
-    The Home class represents the main user interface of the application..
-    """
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
 
-    def __init__(self, **properties):
-        """
-        Initialize the Home form. Handle login, setup user accounts,
-        and render the main graph.
-        """
-        # Set Form properties and Data Bindings
-        self.init_components(**properties)
+    # Initialize accounts dict
+    self.accounts = None
 
-        # Initialize accounts dictionary
-        self.accounts = None
+    # Handle Login
+    anvil.users.login_with_form()
 
-        # Handle user login
-        anvil.users.login_with_form()
+    if anvil.server.call('is_first_login'):
+      t = anvil.TextBox(placeholder='Account Name')
+      anvil.alert(content=t, title='Hi, please enter the Name of your first Account :)', dismissible=False, buttons=[('Create')], large=True)
+      anvil.server.call('setup_user', t.text)
+      
+      
+    
+    # get Users accounts
+    self.update_accounts()
+    # initialize main graph
+    self.update_main_graph()
 
-        # Perform first-time user setup if applicable
-        if anvil.server.call('is_first_login'):
-            account_name_input = TextBox(placeholder='Account Name')
-            anvil.alert(
-                content=account_name_input,
-                title='Welcome! Please enter the name of your first account:',
-                dismissible=False,
-                buttons=[('Create')],
-                large=True,
-            )
-            anvil.server.call('setup_user', account_name_input.text)
+  def button_now_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('Home')
 
-        # Retrieve and display user's accounts
-        self.update_accounts()
+  def button_transaction_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('Transactions')
 
-        # Initialize the main graph
-        self.update_main_graph()
+  def button_insights_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('Insights')
 
-    def button_now_click(self, **event_args):
-        """Navigate to the Home form."""
-        open_form('Home')
+  def button_setting_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('Settings')
 
-    def button_transaction_click(self, **event_args):
-        """Navigate to the Transactions form."""
-        open_form('Transactions')
+  def button_income_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('Transaction_Form', type='income')
 
-    def button_insights_click(self, **event_args):
-        """Navigate to the Insights form."""
-        open_form('Insights')
+  def button_transfer_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('Transaction_Form', type='transfer')
 
-    def button_setting_click(self, **event_args):
-        """Navigate to the Settings form."""
-        open_form('Settings')
+  def button_expense_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('Transaction_Form', type='expense')
 
-    def button_income_click(self, **event_args):
-        """Open the Transaction_Form with the 'income' type."""
-        open_form('Transaction_Form', type='income')
+  def dp_accounts_change(self, **event_args):
+    """This method is called when an item is selected"""
+    selected_account_id = self.dp_accounts.selected_value
+    anvil.server.call('set_account_setting', selected_account_id)
+    self.update_main_graph()
 
-    def button_transfer_click(self, **event_args):
-        """Open the Transaction_Form with the 'transfer' type."""
-        open_form('Transaction_Form', type='transfer')
 
-    def button_expense_click(self, **event_args):
-        """Open the Transaction_Form with the 'expense' type."""
-        open_form('Transaction_Form', type='expense')
+  def update_accounts(self):
+    self.accounts = anvil.server.call('get_user_accounts')
 
-    def dp_accounts_change(self, **event_args):
-        """
-        Update the current account setting when the user selects
-        a different account from the dropdown menu.
-        """
-        selected_account_id = self.dp_accounts.selected_value
-        anvil.server.call('set_account_setting', selected_account_id)
-        self.update_main_graph()
+    self.drop_down_1.items = [(account['name'], account['id']) for account in self.accounts]
 
-    def update_accounts(self):
-        """
-        Fetch and update the user's accounts in the dropdown menu.
-        Set the currently selected account.
-        """
-        self.accounts = anvil.server.call('get_user_accounts')
-        self.dp_accounts.items = [(account['name'], account['id']) for account in self.accounts]
+    if self.accounts:
+      for account in self.accounts:
+        if account['id'] == anvil.server.call('get_current_account_id'):
+          self.dp_accounts.selected_value = account['id']
 
-        if self.accounts:
-            current_account_id = anvil.server.call('get_current_account_id')
-            for account in self.accounts:
-                if account['id'] == current_account_id:
-                    self.dp_accounts.selected_value = account['id']
+  def update_main_graph(self):
+    data = anvil.server.call('get_daily_total_data')
+    dates = data['dates']
+    totals = data['net_totals']
 
-    def update_main_graph(self):
-        """
-        Update the main graph with daily total data.
-        The graph displays net totals with color-coded markers
-        indicating changes in values.
-        """
-        data = anvil.server.call('get_daily_total_data')
-        dates = data['dates']
-        totals = [int(round(total)) for total in data['net_totals']]
+    totals = [int(round(total)) for total in totals]
 
-        # Determine marker colors based on total trends
-        marker_colors = []
-        for i, total in enumerate(totals):
-            if i == 0:
-                marker_colors.append('red' if total < 0 else 'black')
+    # Initialize a list for marker colors
+    marker_colors = []
+    
+    # Determine colors for each marker based on the specified conditions
+    for i in range(len(totals)):    
+        if i == 0:  # First marker
+            if totals[i] < 0:  
+                marker_colors.append('red')   
+            else: 
+                marker_colors.append('black')
+        elif i == 1:  # Second marker
+            if totals[i] > totals[i - 1]:   
+                marker_colors.append('green')   
+            elif totals[i] == totals[i - 1]: 
+                marker_colors.append('black')
             else:
-                marker_colors.append(
-                    'green' if total > totals[i - 1] else
-                    'red' if total < totals[i - 1] else
-                    'black'
-                )
+                marker_colors.append('red')
+        elif i == 2:  # Third marker
+            if totals[i] > totals[i - 1]:
+                marker_colors.append('green')
+            elif totals[i] == totals[i - 1]:
+                marker_colors.append('black')
+            else:
+                marker_colors.append('red')
+              
+    self.plot_now.data = [go.Scatter(
+      x=dates, 
+      y=totals, 
+      text=totals, 
+      textposition='center', 
+      textfont=dict(
+        size=18,
+        color='white',
+      ), 
+      marker=dict(size=40, color=marker_colors), 
+      mode='lines+markers+text')]
 
-        # Update Plotly graph data
-        self.plot_now.data = [
-            go.Scatter(
-                x=dates,
-                y=totals,
-                text=totals,
-                textposition='center',
-                textfont=dict(size=18, color='white'),
-                marker=dict(size=40, color=marker_colors),
-                mode='lines+markers+text',
-            )
-        ]
+    self.plot_now.layout = go.Layout(
+    showlegend=False,  # Hide the legend if not needed
+    xaxis=dict(
+        showgrid=False,   # Hide grid lines
+        zeroline=False,   # Hide the zero line
+        showline=False,   # Hide the axis line
+        title='',         # Optionally remove the title
+        tickvals=dates,
+        ticktext=dates,
+        ticks='outside',
+      ),
+      yaxis=dict(
+        showgrid=False,   # Hide grid lines
+        zeroline=False,   # Hide the zero line
+        showline=False,   # Hide the axis line
+        title='',         # Optionally remove the title
+        showticklabels=False,
+      ),
+      plot_bgcolor='rgba(255,255,255,0)',  # Optional: Set background color to transparent
+      paper_bgcolor='rgba(0,0,0,0.2)',
+    )
 
-        # Configure graph layout
-        self.plot_now.layout = go.Layout(
-            showlegend=False,
-            xaxis=dict(
-                showgrid=False,
-                zeroline=False,
-                showline=False,
-                tickvals=dates,
-                ticktext=dates,
-                ticks='outside',
-            ),
-            yaxis=dict(
-                showgrid=False,
-                zeroline=False,
-                showline=False,
-                showticklabels=False,
-            ),
-            plot_bgcolor='rgba(255,255,255,0)',
-            paper_bgcolor='rgba(0,0,0,0.2)',
-        )
-        self.plot_now.interactive = False
+    self.plot_now.interactive = False
 
-    def recalc_daily_click(self, **event_args):
-        """Trigger a recalculation of daily totals on the server."""
-        anvil.server.call('test_recalc')
+  def plot_now_show(self, **event_args):
+    """This method is called when the Plot is shown on the screen"""
+    pass
+
+  def recalc_daily_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.server.call('test_recalc')
+ 
