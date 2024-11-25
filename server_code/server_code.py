4b631f96b8086facf93c3b3bfd72f34f32ca7fcd
blackforestlphd@gmail.com <blackforestlphd@gmail.com>
2024-11-25 21:30:23 +0000

    Edited server_code

--- server_code/server_code.py
+++ server_code/server_code.py
@@ -503,7 +503,7 @@ def get_month_range():
         first_day = datetime(year, month, 1)
         # Get the last day of the month
         last_day = datetime(year, month, calendar.monthrange(year, month)[1])
-        month_ranges.append(((first_day), (last_day+timedelta(days=1))))
+        month_ranges.append(((first_day), (last_day)))
     return month_ranges
 
 @anvil.server.callable
