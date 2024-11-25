bff9fcb29ab90a1942f63b11a8dcf5b9207d823c
blackforestlphd@gmail.com <blackforestlphd@gmail.com>
2024-11-25 21:38:04 +0000

    Edited server_code

--- server_code/server_code.py
+++ server_code/server_code.py
@@ -122,7 +122,7 @@ def is_2_ic_oc_month(accounts=[]):
         month = first_day.month
         daily_rows = app_tables.dailytotals.search(
           account=app_tables.settings.get(user=anvil.users.get_user())['current_account'],
-          date=q.between(first_day, last_day)
+          date=q.all_of(q.greater_than_or_equal_to(first_day), q.less_than_or_equal_to(last_day))
         )
         month_dict = {}
     
